af245c9422966225a3bfdffef2e0a30587353a3e
Chlamydomonos <2457242458@qq.com>
2025-01-24 22:44:42 +0800

    1.1.11

--- src/App.vue
+++ src/App.vue
@@ -2,24 +2,34 @@
     <div class="outer-frame">
         <div>角色名称: {{ charName }}</div>
         <hr />
-        <!-- Wrapper API 用法 -->
-        <div v-html="wrapperApi.renderMessage(message)"></div>
+        <div v-html="rendered"></div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ProjectHelper } from '@sillytavern-vue-frontend/project-helper';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 // ProjectHelper的用法
 const message = ref(ProjectHelper.initialMessage());
 const wrapperApi = ProjectHelper.wrapperApi();
 const eventEmitter = ProjectHelper.eventEmitter();
-const tavernContext = ProjectHelper.tavernContext();
+
+// tavernContext函数会再返回一个函数，因为每次调用获得的SillyTavern上下文可能会有变化
+const tavernContextGetter = ProjectHelper.tavernContext();
+const tavernContext = tavernContextGetter();
 
 // 事件系统用法
 eventEmitter.on('messageUpdated', (newMes) => {
     message.value = newMes;
+});
+
+//Wrapper API用法
+const rendered = computed(() => {
+    // return ProjectHelper.wrapperApi().renderMessage(message.value);
+    // 以上一句是错误的，因为ProjectHelper通过Vue依赖注入获得数据，而Vue依赖注入只能在setup上下文中调用
+    // 正确做法是用一个变量缓存ProjectHelper返回的值并使用
+    return wrapperApi.renderMessage(message.value);
 });
 
 // SillyTavern上下文用法
